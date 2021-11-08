22636eb84e75ca0f1a88f20726fb7e5375c138ab
Gabriel Medeiros <gjmedeiros@minsait.com>
2021-11-07 23:31:15 -0300

    Comecando a Landing

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, Text, TouchableHighlight, View } from 'react-native';
+
+import styles from './styles';
+
+import landingImg from '../../assets/images/landing.png';
+import gpsImg from '../../assets/images/icons/gps.png';
 
 function Landing() {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <Image source={landingImg} style={styles.banner} />
+
+      <Text style={styles.title}>
+        Seja Bem Vindo, {'\n'}
+        <Text style={styles.titleBold}>Qual é a sua Cidade ?</Text>
+      </Text>
+    </View>
+  );
 }
 
 export default Landing;
